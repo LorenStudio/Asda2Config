cf2b4047dd1ad78ff301d9c0f9e41c2a09709fe8
LorenStudio <79727598+LorenStudio@users.noreply.github.com>
2021-02-27 14:35:47 -0300

    Source Code

---
+++ Src/ConfigAsda/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace ConfigAsda
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.WinMode = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // WinMode
+            // 
+            this.WinMode.AutoSize = true;
+            this.WinMode.Location = new System.Drawing.Point(161, 104);
+            this.WinMode.Name = "WinMode";
+            this.WinMode.Size = new System.Drawing.Size(15, 14);
+            this.WinMode.TabIndex = 0;
+            this.WinMode.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(117, 134);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 31);
+            this.button1.TabIndex = 3;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1024 X 768",
+            "1280 X 768",
+            "1280 X 800",
+            "1280 X 960",
+            "1280 X 1024",
+            "1366 X 768",
+            "1680 X 1050",
+            "1920 X 1080"});
+            this.comboBox1.Location = new System.Drawing.Point(145, 41);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(105, 21);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.Text = "1024 X 768";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "NONE",
+            "X2",
+            "X4",
+            "X8"});
+            this.comboBox2.Location = new System.Drawing.Point(145, 71);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(105, 21);
+            this.comboBox2.TabIndex = 8;
+            this.comboBox2.Text = "NONE";
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(349, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(23, 22);
+            this.button2.TabIndex = 9;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(378, 172);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.WinMode);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Asda2CFG";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox WinMode;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Src/ConfigAsda/Form1.cs
@@ -0,0 +1,466 @@
+﻿////////////////////////////////////////////////
+//             LORENSTUDIO                     /
+//          By Rodrigo(Lucifer)                /
+//          Whats: (22)98186-8617              /
+//        Email: suporte@lorenstudio.com       /
+////////////////////////////////////////////////
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+using System.Windows;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.Win32;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace ConfigAsda
+{
+    public partial class Form1 : Form
+    {
+        bool IsMinimized;
+        int Resolut;
+        public Form1()
+        {
+            InitializeComponent();
+            CheckMinimized();
+        }
+
+        private void CheckMinimized() //VERIFICA SE A CHECKBOX FOI SELECIONADA OU NÃO.
+        {
+            string str = ".\\\\system\\NewConfig.ini";
+            try
+            {
+                if (System.IO.File.ReadAllLines(str)[8] == "Window_Mode    =TRUE")
+                {
+                    Common.lineChanger("Window_Mode    =TRUE", str, 9);
+                    IsMinimized = true;
+                    WinMode.Checked = true;
+                }
+                else
+                {
+                    Common.lineChanger("Window_Mode    =FALSE", str, 9);
+                    IsMinimized = false;
+                    WinMode.Checked = false;
+                }
+
+            }
+            catch
+            {
+                if (!System.IO.File.Exists(str))
+                {
+                    string[] contents = new string[30]
+                    {
+"[System]",
+"ResolutionX        =",
+"ResolutionY        =",
+"ResolutionBPP    =",
+"Monitor_Refresh  =",
+"Depth_Bit              =",
+"TextureFilter        =",
+"Enable_Light       =",
+"Window_Mode    =TRUE",
+"Texture_Minimum =",
+"FPS_Show         =",
+"First_Start          ",
+"Language           =",
+"FontSize=",
+"\n",
+"[Graphic]",
+"GraphicCustumize =",
+"Grass_Show       =",
+"Soft_Horizon       =",
+"Character_Shade =",
+"Terrain_Rendering =",
+"Water_Reflect      =",
+"TerrainBright        =",
+"TerrainShadow     =",
+"WeatherEffect     =",
+"Shadow_Quality  =",
+"Camera_Dis        =",
+"MultiSampling_Type=0",
+"OutLine_Rendering=",
+"Object_View_Distance="
+                    };
+                    System.IO.File.WriteAllLines(str, contents);
+                }
+                else
+                    Common.lineChanger("Window_Mode    =TRUE", str, 9);
+            }
+        }
+
+
+
+
+
+
+        private void SetWinMode() //SETA EM MODO JANELA
+        {
+            string str = ".\\\\system\\NewConfig.ini";
+            {
+                if (System.IO.File.ReadAllLines(str)[8] == "Window_Mode    =FALSE")
+                {
+                    Common.lineChanger("Window_Mode    =TRUE", str, 9);
+                    IsMinimized = true;
+                }
+                else
+                {
+                    Common.lineChanger("Window_Mode    =FALSE", str, 9);
+                    IsMinimized = false;
+                }
+
+            }
+        }
+
+        private void Resolu1024() //MUDA A RESOLUÇÃO 
+        {
+            string str = ".\\\\system\\NewConfig.ini";
+            {
+                if (System.IO.File.ReadAllLines(str)[2] == "ResolutionX        =0")
+                {
+                    Common.lineChanger("ResolutionX        =1024", str, 2);
+                    Common.lineChanger("ResolutionY        =768", str, 3);
+
+                    Resolut = 1024;
+                    Resolut = 768;
+                }
+                else
+                {
+                    Common.lineChanger("ResolutionX        =1024", str, 2);
+                    Common.lineChanger("ResolutionY        =768", str, 3);
+                    Resolut = 1024;
+                    Resolut = 768;
+                }
+
+            }
+        }
+
+
+        private void Resolu1280() //MUDA A RESOLUÇÃO 
+        {
+            string str = ".\\\\system\\NewConfig.ini";
+            {
+                if (System.IO.File.ReadAllLines(str)[2] == "ResolutionX        =0")
+                {
+                    Common.lineChanger("ResolutionX        =1280", str, 2);
+                    Common.lineChanger("ResolutionY        =768", str, 3);
+
+                    Resolut = 1280;
+                    Resolut = 768;
+                }
+                else
+                {
+                    Common.lineChanger("ResolutionX        =1280", str, 2);
+                    Common.lineChanger("ResolutionY        =768", str, 3);
+                    Resolut = 1280;
+                    Resolut = 768;
+                }
+
+            }
+        }
+
+        private void Resolu12802() //MUDA A RESOLUÇÃO 
+        {
+            string str = ".\\\\system\\NewConfig.ini";
+            {
+                if (System.IO.File.ReadAllLines(str)[2] == "ResolutionX        =0")
+                {
+                    Common.lineChanger("ResolutionX        =1280", str, 2);
+                    Common.lineChanger("ResolutionY        =800", str, 3);
+
+                    Resolut = 1280;
+                    Resolut = 800;
+                }
+                else
+                {
+                    Common.lineChanger("ResolutionX        =1280", str, 2);
+                    Common.lineChanger("ResolutionY        =800", str, 3);
+                    Resolut = 1280;
+                    Resolut = 800;
+                }
+
+            }
+        }
+
+
+        private void Resolu12803() //MUDA A RESOLUÇÃO 
+        {
+            string str = ".\\\\system\\NewConfig.ini";
+            {
+                if (System.IO.File.ReadAllLines(str)[2] == "ResolutionX        =0")
+                {
+                    Common.lineChanger("ResolutionX        =1280", str, 2);
+                    Common.lineChanger("ResolutionY        =960", str, 3);
+
+                    Resolut = 1280;
+                    Resolut = 960;
+                }
+                else
+                {
+                    Common.lineChanger("ResolutionX        =1280", str, 2);
+                    Common.lineChanger("ResolutionY        =960", str, 3);
+                    Resolut = 1280;
+                    Resolut = 960;
+                }
+
+            }
+        }
+
+        private void Resolu12804() //MUDA A RESOLUÇÃO 
+        {
+            string str = ".\\\\system\\NewConfig.ini";
+            {
+                if (System.IO.File.ReadAllLines(str)[2] == "ResolutionX        =0")
+                {
+                    Common.lineChanger("ResolutionX        =1280", str, 2);
+                    Common.lineChanger("ResolutionY        =1024", str, 3);
+
+                    Resolut = 1280;
+                    Resolut = 1024;
+                }
+                else
+                {
+                    Common.lineChanger("ResolutionX        =1280", str, 2);
+                    Common.lineChanger("ResolutionY        =1024", str, 3);
+                    Resolut = 1280;
+                    Resolut = 1024;
+                }
+
+            }
+        }
+
+
+        private void Resolu1366() //MUDA A RESOLUÇÃO 
+        {
+            string str = ".\\\\system\\NewConfig.ini";
+            {
+                if (System.IO.File.ReadAllLines(str)[2] == "ResolutionX        =0")
+                {
+                    Common.lineChanger("ResolutionX        =1366", str, 2);
+                    Common.lineChanger("ResolutionY        =768", str, 3);
+
+                    Resolut = 1366;
+                    Resolut = 768;
+                }
+                else
+                {
+                    Common.lineChanger("ResolutionX        =1366", str, 2);
+                    Common.lineChanger("ResolutionY        =768", str, 3);
+                    Resolut = 1366;
+                    Resolut = 768;
+                }
+
+            }
+        }
+
+
+
+        private void Resolu1680() //MUDA A RESOLUÇÃO 
+        {
+            string str = ".\\\\system\\NewConfig.ini";
+            {
+                if (System.IO.File.ReadAllLines(str)[2] == "ResolutionX        =0")
+                {
+                    Common.lineChanger("ResolutionX        =1680", str, 2);
+                    Common.lineChanger("ResolutionY        =1050", str, 3);
+
+                    Resolut = 1680;
+                    Resolut = 1050;
+                }
+                else
+                {
+                    Common.lineChanger("ResolutionX        =1680", str, 2);
+                    Common.lineChanger("ResolutionY        =1050", str, 3);
+                    Resolut = 1680;
+                    Resolut = 1050;
+                }
+
+            }
+        }
+
+
+        private void Resolu1920() //MUDA A RESOLUÇÃO  
+        {
+            string str = ".\\\\system\\NewConfig.ini";
+            {
+                if (System.IO.File.ReadAllLines(str)[2] == "ResolutionX        =0")
+                {
+                    Common.lineChanger("ResolutionX        =1920", str, 2);
+                    Common.lineChanger("ResolutionY        =1080", str, 3);
+
+                    Resolut = 1920;
+                    Resolut = 1080;
+                }
+                else
+                {
+                    Common.lineChanger("ResolutionX        =1920", str, 2);
+                    Common.lineChanger("ResolutionY        =1080", str, 3);
+                    Resolut = 1920;
+                    Resolut = 1080;
+                }
+
+            }
+        }
+
+
+        private void AliasingNone() //SETA NULO NO ANTIALIASING
+        {
+            string str = ".\\\\system\\NewConfig.ini";
+            {
+                if (System.IO.File.ReadAllLines(str)[27] == "MultiSampling_Type        =0")
+                {
+                    Common.lineChanger("MultiSampling_Type        =0", str, 28);
+                    Resolut = 0;
+                }
+                else
+                {
+                    Common.lineChanger("MultiSampling_Type        =0", str, 28);
+                    Resolut = 0;
+                }
+
+            }
+        }
+
+        private void AliasingX2() //SETA X2 NO ANTIALIASING
+        {
+            string str = ".\\\\system\\NewConfig.ini";
+            {
+                if (System.IO.File.ReadAllLines(str)[27] == "MultiSampling_Type        =0")
+                {
+                    Common.lineChanger("MultiSampling_Type        =1", str, 28);
+                    Resolut = 1;
+                }
+                else
+                {
+                    Common.lineChanger("MultiSampling_Type        =1", str, 28);
+                    Resolut = 1;
+                }
+
+            }
+        }
+
+
+        private void AliasingX4() //SETA X4 NO ANTIALIASING
+        {
+            string str = ".\\\\system\\NewConfig.ini";
+            {
+                if (System.IO.File.ReadAllLines(str)[27] == "MultiSampling_Type        =0")
+                {
+                    Common.lineChanger("MultiSampling_Type        =2", str, 28);
+                    Resolut = 2;
+                }
+                else
+                {
+                    Common.lineChanger("MultiSampling_Type        =2", str, 28);
+                    Resolut = 2;
+                }
+
+            }
+        }
+
+
+        private void AliasingX8() //SETA X8 NO ANTIALIASING
+        {
+            string str = ".\\\\system\\NewConfig.ini";
+            {
+                if (System.IO.File.ReadAllLines(str)[27] == "MultiSampling_Type        =0")
+                {
+                    Common.lineChanger("MultiSampling_Type        =3", str, 28);
+                    Resolut = 3;
+                }
+                else
+                {
+                    Common.lineChanger("MultiSampling_Type        =3", str, 28);
+                    Resolut = 3;
+                }
+
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckMinimized();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                Resolu1024();
+            }
+            if (comboBox1.SelectedIndex == 1)
+            {
+                Resolu1280();
+            }
+            if (comboBox1.SelectedIndex == 2)
+            {
+                Resolu12802();
+            }
+            if (comboBox1.SelectedIndex == 3)
+            {
+                Resolu12803();
+            }
+            if (comboBox1.SelectedIndex == 4)
+            {
+                Resolu12804();
+            }
+            if (comboBox1.SelectedIndex == 5)
+            {
+                Resolu1366();
+            }
+            if (comboBox1.SelectedIndex == 6)
+            {
+                Resolu1680();
+            }
+            if (comboBox1.SelectedIndex == 7)
+            {
+                Resolu1920();
+            }
+
+            if (comboBox2.SelectedIndex == 0)
+            {
+               AliasingNone();
+            }
+
+            if (comboBox2.SelectedIndex == 1)
+            {
+                AliasingX2();
+            }
+
+            if (comboBox2.SelectedIndex == 2)
+            {
+                AliasingX2();
+            }
+
+            if (comboBox2.SelectedIndex == 3)
+            {
+                AliasingX4();
+            }
+
+            if (comboBox2.SelectedIndex == 4)
+            {
+                AliasingX8();
+            }
+
+            SetWinMode();
+
+            MessageBox.Show("Configuração Salva:" + "\n  " + "Modo Janela:" + " " + WinMode.Checked + "\n  " + "Resolução:" + " " + comboBox1.SelectedItem + "\n " + "Anti Aliasing:" + " " + comboBox2.SelectedItem);
+
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
